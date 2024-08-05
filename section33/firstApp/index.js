75d530afdd75a2a4fef67c4b215904e83502088f
NamKungSujeong <nksj98@naver.com>
2024-08-05 23:13:51 +0900

    express 라우팅 기초

--- section33/firstApp/index.js
+++ section33/firstApp/index.js
@@ -9,10 +9,32 @@ const app = express();
 // express는 그걸 파싱해서 전달할 객체로 만들고 콜백의 첫번째 인수로 전달해줌
 
 // 요청이 어디서 들어온든 상관없이 콜백 함수 실행
-app.use((req, res) => {
-  console.log("WE GOT A NEW REQUEST!!");
-  res.send("HELLO");
-  // res.send : HTTP 응답을 보내고 생성
+// app.use((req, res) => {
+//   console.log("WE GOT A NEW REQUEST!!");
+//   res.send("HELLO");
+//   // res.send : HTTP 응답을 보내고 생성
+// });
+
+app.get("/", (req, res) => {
+  res.send("This is the home page");
+});
+
+app.get("/cats", (req, res) => {
+  res.send("MEOW!!");
+});
+
+app.post("/cats", (req, res) => {
+  res.send("/cats Post Request");
+});
+
+app.get("/dogs", (req, res) => {
+  res.send("WOOF!!");
+});
+
+app.get("*", (req, res) => {
+  // 존재하지 않는 라우트를 요청할 때
+  // 맨 위에 있을 경우 그 아래 코드는 모두 무시하기 때문에 마지막에 작성하는 것이 중요
+  res.send("I don't know that path");
 });
 
 app.listen(3000, () => {
