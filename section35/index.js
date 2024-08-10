48ce129496c6c77c2e3ebb04e4031e29dc7acf6b
NamKungSujeong <nksj98@naver.com>
2024-08-10 20:47:25 +0900

    365 : 요청구문 분석하기

--- section35/index.js
+++ section35/index.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// req.body의 기본값은 undefined이기 떄문에 암호화된 폼 정보를 분석해 줄 필요가 있음
+// request body를 URL 암호화 데이터로 분석할 미들웨어를 사용
+
 app.get("/tacos", (req, res) => {
   res.send("Get /tacos response");
 });
 
 app.post("/tacos", (req, res) => {
-  res.send("Post /tacos response");
+  const { meat, qty } = req.body;
+  res.send(`here are your ${qty} ${meat} tacos`);
 });
 
 app.listen(3000, () => {
