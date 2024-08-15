9486c574cd2fc9e9fb808096ea6f0398d64ccdda
NamKungSujeong <nksj98@naver.com>
2024-08-15 23:20:27 +0900

    370 : redirect 설정

--- section35/index.js
+++ section35/index.js
@@ -28,7 +28,7 @@ app.get("/comments/new", (req, res) => {
 app.post("/comments", (req, res) => {
   const { username, comment } = req.body;
   comments.push({ username, comment });
-  res.send("It works!");
+  res.redirect("/comments");
 });
 
 app.get("/tacos", (req, res) => {
